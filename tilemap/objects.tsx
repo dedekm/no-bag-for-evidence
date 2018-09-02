982495f41f8a46c54743a195af0719ebd4451768
Martin Dedek <martin.dedek@sinfin.cz>
2018-09-02 14:40:15 +0200

    + tilemap & tilesets files, objects csv [wip]

---
+++ tilemap/objects.tsx
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="objects" tilewidth="32" tileheight="32" tilecount="3" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="1">
+  <image width="32" height="32" source="../public/images/car.png"/>
+ </tile>
+ <tile id="2">
+  <image width="32" height="32" source="../public/images/key.png"/>
+ </tile>
+ <tile id="3">
+  <image width="32" height="32" source="../public/images/hero.png"/>
+ </tile>
+</tileset>
